12486cb2cb68ef4ec5d7c87c4a7f8ce54315d2c7
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-05-18 23:10:37 +0530

    Working pass on join us app!

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const mysql = require("mysql");
 const express = require("express");
+const path = require('path');
+const bodyParser  = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 const { 
   MYSQL_HOST: host, 
   MYSQL_USER: user, 
@@ -11,11 +17,20 @@ const {
 const dbConnection = mysql.createConnection({ host, user, database, password });
 
 app.get("/", (request, response) => {
-  const query = "SELECT COUNT(*) AS total_count FROM join_us_app.users;";
-  dbConnection.query(query, function executeQuery(error, results, fields) {
+  const selectQuery = "SELECT COUNT(*) AS total_count FROM join_us_app.users;";
+  dbConnection.query(selectQuery, function executeQuery(error, results, fields) {
     if (error) throw error;
     const { total_count } = results[0];
-    response.send(`We have ${total_count} users`);
+    response.render("home", { total_count });
+  });
+});
+
+app.post("/register", (request, response) => {
+  const person = { email: request.body.email };
+  const insertQuery = "INSERT INTO users SET ?";
+  dbConnection.query(insertQuery, person, function(error, result) {
+      if (error) throw error;
+      response.redirect("/");
   });
 });
 
