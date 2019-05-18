383b13124e328f1e76e58114b40bc45d46842fb4
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-05-18 22:16:12 +0530

    Add express and starting code on same!

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 const mysql = require("mysql");
+const express = require("express");
 
+const app = express();
 const { 
   MYSQL_HOST: host, 
   MYSQL_USER: user, 
   MYSQL_DATABASE: database, 
   MYSQL_PASSWORD: password 
 } = process.env;
-
-console.log({ host, user, database, password });
-
 const dbConnection = mysql.createConnection({ host, user, database, password });
-const query = `
-  SELECT 
-    COUNT(*) AS total_count 
-  FROM join_us_app.users;
-`;
-dbConnection.query(query, function executeQuery(error, results, fields) {
-  if (error) throw error;
-  const { total_count } = results[0];
-  console.log({ total_count });
+
+app.get("/", (request, response) => {
+  const query = "SELECT COUNT(*) AS total_count FROM join_us_app.users;";
+  dbConnection.query(query, function executeQuery(error, results, fields) {
+    if (error) throw error;
+    const { total_count } = results[0];
+    response.send(`We have ${total_count} users`);
+  });
 });
 
-dbConnection.end();
+app.listen(8080, () => {
+  console.log("Server running on 8080");
+});
